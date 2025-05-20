c120e834301c3e7921716b4d8c9d10e0c88724d5
iftitahputri <24iftitahmhs.usk.ac.id>
2025-05-20 20:44:59 +0700

    komentar

--- soal2.c
+++ soal2.c
@@ -3,25 +3,26 @@
 #include <string.h>
 
 typedef struct {
-    int *data;      
-    int size;      
+    int *data;     //pointer ke elemen array
+    int size;     //jumlah elemen di array 
 }array;
 
-array *groupArray = NULL;  
-int *outputArray = NULL;
-int count = 0;  
-int outputCount = 0; 
+array *groupArray = NULL;  //array dinamis dari semua group
+int *outputArray = NULL;   //array untuk meyimpan output
+int groupCount = 0;        //jumlah group yang ada
+int outputCount = 0;       //jumlah output yang disimpan
 
+//deklarasi fungsi
 void new();
 void add(int, int, int);
 void out(int, int);
 
 int main() {
-    int Q;
+    int Q; //total command yang akan diterima
     printf("Masukkan jumlah command: ");
     scanf("%d", &Q);
     
-    new();
+    new(); // buat group array kosong
     
     printf("Masukkan %d command\n", Q);
     char command[10];
@@ -43,51 +44,58 @@ int main() {
         }
         else{
             printf("Input tidak valid");
-            Q += 1;
+            i--;
             continue;
         }
     }
-    
-    for (int i = 0; i < outputCount; i++) {
-        printf("%d\n", outputArray[i]);
-    }
-    free(outputArray);
 
-    for (int i = 0; i < count; i++) {
+    //cetak output
+    for (int i = 0; i < outputCount; i++) {
+    printf("%d\n", outputArray[i]);
+    }
+ 
+    //membersihkan memori outputarray
+    free(outputArray);
+    
+    //membersihkan memori grouparray
+    for (int i = 0; i < groupCount; i++) {
         free(groupArray[i].data);
     }
     free(groupArray);
 
-
     return 0;
 }
 
+//fungsi untuk menambahkan group array baru
 void new(){
-    groupArray = realloc(groupArray, (count + 1) * sizeof(array));
-    groupArray[count].data = NULL;
-    groupArray[count].size = 0;
-    count++;
+    groupArray = realloc(groupArray, (groupCount + 1) * sizeof(array));
+    groupArray[groupCount].data = NULL;
+    groupArray[groupCount].size = 0;
+    groupCount++;
 }
 
+//fungsi untuk menambahkan elemen ke array yang sudah ditambah
 void add(int l, int x, int y) {
-    int i = l - 1;
-
-    if (i >= count || i < 0) {
+    int i = l - 1; 
+    //mengecek group ada atau tidak
+    if (i >= groupCount || i < 0) {
         printf("Group %d tidak ditemukan.\n", l);
         return;
     }
 
     groupArray[i].data = realloc(groupArray[i].data, (groupArray[i].size + y) * sizeof(int));
-    
     for (int j = 0; j < y; j++) {
         groupArray[i].data[groupArray[i].size + j] = x;
     }
     groupArray[i].size += y;
 }
 
+//fungsi untuk menyimpan nilai yang akan diprint
 void out(int l, int z) {
     int i = l - 1;
-    if (i >= count || i < 0 || z < 1 || z > groupArray[i].size) {
+
+    //cek indeks
+    if (i >= groupCount || i < 0 || z < 1 || z > groupArray[i].size) {
         printf("Index tidak valid.\n");
         return;
     }
