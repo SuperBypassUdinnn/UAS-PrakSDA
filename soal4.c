571c18d4347b027bf2d05595cf535659d1fa2d80
Teuku Fikram Al Syahbanna <teukufikramalsyahbanna@gmail.com>
2025-05-20 09:39:02 +0700

    Inisialisasi program stack sama struktur fungsi dasarnya

---
+++ soal4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 10000
+
+int stack[MAX];
+int top = -1;
+
+// fungsinya add(push) ditambahin nanti
+void add(int X, int y) {}
+
+// fungsinya del(pop) ditambahin nanti
+void del(int Y) {}
+
+void adx(int D) {}
+void dex(int D) {}
+
+int main() {
+    int Q;
+    scanf("%d", &Q);
+
+    char cmd[10];
+    int X, Y;
+
+    for (int i = 0, i < Q, i++)
+        scanf("%s", cmd);
+        // Logikanya ditambah nanti
+        return 0;
+}
